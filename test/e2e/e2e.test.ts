50037f830f74b1de70d3ea9a50a42b3e3e16b3c5
Peter Motzko <info@pmoscode.de>
2021-02-14 00:45:49 +0100

    e2e test extended

--- test/e2e/e2e.test.ts
+++ test/e2e/e2e.test.ts
@@ -1,65 +1,80 @@
 import {Worker} from '../../lib/worker';
 import path from 'path';
 import * as fs from 'fs';
-// import assert from 'assert';
+import assert from 'assert';
 
 
-// const fixturesRoot = path.join(__dirname, 'fixtures');
+const fixturesRoot = path.join(__dirname, 'fixtures');
 const testRoot = path.join(__dirname, 'app');
-// const pkgName = 'package.json';
+const pkgName = 'package.json';
 const bkpName = '.package.json';
-//
-// const fixtures = {
-//     'dev': fs.readFileSync(path.join(fixturesRoot, 'package-dev.json'), 'utf8'),
-//     'test': fs.readFileSync(path.join(fixturesRoot, 'package-test.json'), 'utf8'),
-//     'base': fs.readFileSync(path.join(fixturesRoot, 'package-base.json'), 'utf8')
-// };
-//
+
+const fixtures = {
+    'dev': fs.readFileSync(path.join(fixturesRoot, 'package-dev.json'), 'utf8'),
+    'test': fs.readFileSync(path.join(fixturesRoot, 'package-test.json'), 'utf8'),
+    'base': fs.readFileSync(path.join(fixturesRoot, 'package-base.json'), 'utf8')
+};
+
 const checkFileExist = (fileName: string) => {
     const file = fs.readFileSync(path.join(testRoot, 'envs', fileName), 'utf8');
     expect(file).toBeDefined();
 };
-//
-// const checkFixtureMatch = (message: string, fileName: string, matchFile: string) => {
-//     it(message, function () {
-//         const file = fs.readFileSync(path.join(testRoot, fileName), 'utf8');
-//         assert.deepStrictEqual(JSON.parse(matchFile), JSON.parse(file));
-//     });
-// };
 
+const checkFixtureMatch = (fileName: string, matchFile: string) => {
+    const file = fs.readFileSync(path.join(testRoot, fileName), 'utf8');
+    assert.deepStrictEqual(JSON.parse(matchFile), JSON.parse(file));
+};
 
 describe('Testing NPB CLI', function () {
+    beforeEach(() => {
+        process.argv = process.argv.slice(0, 2)
+        jest.spyOn(process, 'cwd').mockReturnValue(testRoot);
+    })
+
     afterEach(() => {
         if (fs.existsSync(path.join(testRoot, 'envs', '.package.json'))) {
-            jest.spyOn(process, 'cwd').mockReturnValue(testRoot);
-            new Worker().start();
-            // fs.unlinkSync(path.join(testRoot, 'env', '.package.json'));
+            fs.copyFileSync(path.join(testRoot, 'envs', '.package.json'), path.join(testRoot, 'package.json'));
+            fs.unlinkSync(path.join(testRoot, 'envs', '.package.json'));
         }
     });
 
     it('should create dev staged package.json and create a backup of original', () => {
-        process.argv.push('test');
-        jest.spyOn(process, 'cwd').mockReturnValue(testRoot);
+        process.argv.push('dev');
 
         const worker: Worker = new Worker();
         worker.start();
 
-        checkFileExist(bkpName)
+        checkFileExist(bkpName);
+        checkFixtureMatch(pkgName, fixtures.dev);
     });
 
-    // checkFileExist('backup package should exist', bkpName);
-    // checkFixtureMatch('package.json should be modified', pkgName, fixtures.staging);
+    it('should create test staged package.json and create a backup of original', () => {
+        process.argv.push('test');
+
+        const worker: Worker = new Worker();
+        worker.start();
+
+        checkFileExist(bkpName);
+        checkFixtureMatch(pkgName, fixtures.test);
+    });
+
+    it('should delete backup package.json if no environment is set', () => {
+        process.argv.push('dev');
+
+        let worker: Worker = new Worker();
+        worker.start();
+
+        checkFileExist(bkpName);
+        checkFixtureMatch(pkgName, fixtures.dev);
+
+        process.argv = process.argv.slice(0, 2)
+
+        worker = new Worker();
+        worker.start();
+
+        if (fs.existsSync(path.join(testRoot, 'envs', bkpName))) {
+            fail('backup should be deleted!')
+        }
+    });
 });
-
-
-// describe('#start() second: use backup', function () {
-//     beforeEach(() => {
-//         // Mock "parseArgs()" from ParsedArgs to return test data
-//     });
-//     afterEach(() => {
-//         fs.unlinkSync(path.join(testRoot, '.package.json'));
-//     });
-//     //beginExec('base');
-//     checkFixtureMatch('package.json should be reset', pkgName, fixtures.base);
-// });
 
