4c88d29c4a43d8fb243608216a640ca8ef550cc9
Peter Motzko <info@pmoscode.de>
2023-05-24 12:22:00 +0200

    fix: change call to ncu

--- pipeline/prepare.sh
+++ pipeline/prepare.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Get latest changes for packages
-RESULT=$(node ./node_modules/npm-check-updates/bin/cli.js --jsonUpgraded -p yarn)
+RESULT=$(ncu --jsonUpgraded -p yarn)
 
 # Count lines (>1 line: json object has changes)
 CHANGES_COUNT=$(echo "$RESULT" | wc -l)
@@ -14,7 +14,7 @@ else
   echo "There are dependency updates available:"
 
   LATEST_VERSION=$(echo "$RESULT" | jq '[. | to_entries[] | .value ] | first' | sed -e 's/^"//' -e 's/"$//')
-  node ./node_modules/npm-check-updates/bin/cli.js -p yarn
+  ncu -p yarn
 
   echo "Sending email notification..."
   ./pipeline/send_mail.sh
