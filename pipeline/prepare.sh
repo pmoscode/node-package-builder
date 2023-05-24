7324ad3a45d6640dd13ec5c2d490d2b79fb32f2e
Peter Motzko <info@pmoscode.de>
2023-05-24 14:41:39 +0200

    chore: remove unused code

--- pipeline/prepare.sh
+++ pipeline/prepare.sh
@@ -12,8 +12,6 @@ if [[ "$CHANGES_COUNT" -eq 1 ]]; then
   echo "Everything is up-tp-date. Nothing to do..."
 else
   echo "There are dependency updates available:"
-
-  LATEST_VERSION=$(echo "$RESULT" | jq '[. | to_entries[] | .value ] | first' | sed -e 's/^"//' -e 's/"$//')
   ncu -p yarn
 
   echo "Sending email notification..."
